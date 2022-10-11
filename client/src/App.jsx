4a465a2b48336dfa277539cf9155d3a7b9881460
Manases <manasesgil95@gmail.com>
2022-10-11 11:43:23 -0500

    small changes on my build

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react'
 import Cookies from 'universal-cookie'; //found all of this on google I'm using something called stream which is a website that provides the API for the chats to work
 
-const apiKey = ''
+const apiKey = 'snsury5jqz56'
 
 const App = () => {
     return (
