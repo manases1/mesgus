1669e9c5c7719e8489738268b11209433f40f2f3
Manases <manasesgil95@gmail.com>
2022-10-12 14:26:58 -0500

    working the side bar channel idea is from discord

--- client/src/components/ChanelListContainer.jsx
+++ client/src/components/ChanelListContainer.jsx
@@ -1,4 +1,18 @@
 import React from 'react'
+import { ChanelList, useChatContext } from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './'
+
+const SideBar = () => (
+    <div className='channel-list__sidebar'>
+        <div className='channel-list__sidebar__icon1'>
+            <div className='icon1__inner'>
+                <img src={icon} alt='icon here' width="30" />
+            </div>
+        </div>
+    </div>
+)
 
 const ChanelListContainer = () => {
   return (
