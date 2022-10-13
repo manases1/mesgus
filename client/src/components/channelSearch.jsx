8e0f8ec79897fa67101a655d2275d471bd0fd746
Manases <manasesgil95@gmail.com>
2022-10-13 15:50:20 -0500

    getting things ready to be able to search

---
+++ client/src/components/channelSearch.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react'
+import { useChatContext } from 'stream-chat-react'
+
+import { SearchIcon } from '../MyAssets'
+
+const channelSearch = () => {
+  return (
+    <div className="channel-search__container">
+        <div className="channel-search__input__wrapper">
+            <div className="channel-search__input__icon">
+                <SearchIcon />
+            </div>
+            <input className="channel-search__input__text" placeholder="Search" type="text" value={query} onChange={onSearch} />
+        </div>
+    </div>
+  )
+}
+
+export default channelSearch
