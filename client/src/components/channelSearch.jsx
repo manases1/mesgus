770d2dc7d988065dc3cb34834ca5856c833e996d
Manases <manasesgil95@gmail.com>
2022-10-13 15:57:33 -0500

    about to import to channel container

--- client/src/components/channelSearch.jsx
+++ client/src/components/channelSearch.jsx
@@ -4,13 +4,37 @@ import { useChatContext } from 'stream-chat-react'
 import { SearchIcon } from '../MyAssets'
 
 const channelSearch = () => {
+    const [query, setQuery] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const getChannels = async (text) => {
+        try {
+            // fetch channels
+        } catch (error) {
+            setQuery('')
+        }
+    }
+
+    const onSearch = (event) => {
+    event.preventDefault();
+
+    setLoading(true);
+    setQuery(event.target.value);
+    getChannels(event.target.value);
+}
+
   return (
     <div className="channel-search__container">
         <div className="channel-search__input__wrapper">
             <div className="channel-search__input__icon">
                 <SearchIcon />
             </div>
-            <input className="channel-search__input__text" placeholder="Search" type="text" value={query} onChange={onSearch} />
+            <input 
+            className="channel-search__input__text" 
+            placeholder="Search" 
+            type="text" 
+            value={query} 
+            onChange={onSearch} />
         </div>
     </div>
   )
