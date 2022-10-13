728bcef68607cd15839788b6de5a0c16139d7006
Manases <manasesgil95@gmail.com>
2022-10-13 18:53:06 -0500

    big updates

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -5,12 +5,73 @@ import axios from 'axios';
 import signinImage from '../MyAssets/signup.jpg'
 
 const Auth = () => {
-    const [isSignup, setIsSignup] = useState(false);
+    const [isSignup, setIsSignup] = useState(true);
+
+    const handleChange = () => {};
+
   return (
     <div className="auth__form-container">
         <div className="auth__form-container_fields">
             <div className="auth__form-container_fields-content">
                 <p>{isSignup ? 'Sign Up' : 'Sign In'}</p>
+                <form onSubmit={() => {}}>
+                    {isSignup && (
+                        <div className="auth__form-container-fields-content_input">
+                            <label htmlFor="fullName">Full Name</label>
+                            <input
+                                name="fullName"
+                                type="text"
+                                placeholder="Full Name"
+                                onChange={handleChange}
+                                required
+                                />
+                        </div>
+                    )}
+                        <div className="auth__form-container-fields-content_input">
+                            <label htmlFor="fullName">Username</label>
+                            <input
+                                name="username"
+                                type="text"
+                                placeholder="Username"
+                                onChange={handleChange}
+                                required
+                                />
+                        </div>
+                        {isSignup && (
+                        <div className="auth__form-container-fields-content_input">
+                            <label htmlFor="phoneNumber">Phone Number</label>
+                            <input
+                                name="phoneNumber"
+                                type="text"
+                                placeholder="Phone Number"
+                                onChange={handleChange}
+                                required
+                                />
+                        </div>
+                    )}
+                        {isSignup && (
+                        <div className="auth__form-container-fields-content_input">
+                            <label htmlFor="avatarURL">Avatar URL</label>
+                            <input
+                                name="avatarURL"
+                                type="text"
+                                placeholder="Avatar URL"
+                                onChange={handleChange}
+                                required
+                                />
+                        </div>
+                    )}
+                    <div className="auth__form-container-fields-content_input">
+                            <label htmlFor="password">Password</label>
+                            <input
+                                name="password"
+                                type="password"
+                                placeholder="Password"
+                                onChange={handleChange}
+                                required
+                                />
+                        </div>
+                </form>
             </div>
         </div>
     </div>
