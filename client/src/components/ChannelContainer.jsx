4480d220d58590d2d00b60dfacdd692237907739
Manases <manasesgil95@gmail.com>
2022-10-14 12:08:57 -0500

    working on the channel container

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Channel, useChatContext } from 'stream-chat-react';
+
+import { ChannelInner, CreateChannel, EditChannel, TeamMessage } from './';
 
 const ChannelContainer = () => {
   return (
-    <div>ChanelContainer</div>
+    <div>
+
+    </div>
   )
 }
 
