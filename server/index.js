f40ea27f5cdf6e7026dd00f58a2fadfd41696ca9
Manases <manasesgil95@gmail.com>
2022-10-14 11:16:16 -0500

    working on mmore auth conttroller(backend)

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
+const authRoutes = require("./routes/auth.js");
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 
@@ -13,5 +15,7 @@ app.use(express.urlencoded());
 app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
+
+app.use('/auth', authRoutes);
 
 app.listen(PORT, () => console.log('listening on port ' + PORT));
